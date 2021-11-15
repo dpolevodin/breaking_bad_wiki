937d97e8790ba788a062c611fd93814e50e7590d
dpolevodin <dpolevodin@gmail.com>
2021-11-15 17:02:03 +0500

    change button in quotes

--- src/components/Quotes/QuotesButton.js
+++ src/components/Quotes/QuotesButton.js
@@ -1,5 +1,5 @@
 const QuotesButton = ({getQuoteByAuthor}) => {
-    return <button className="blockquote__button" onClick={getQuoteByAuthor}>Get quote by author</button>
+    return <button className="blockquote__button" onClick={getQuoteByAuthor}>Get quotes by author</button>
 }
 
 export default QuotesButton
